2479dccb3c722bc5821ed82931c024e3f94d3619
tunde-blades <82004820+tunde-blades@users.noreply.github.com>
2022-11-16 21:26:27 +0100

    updated

--- src/layout/sidebar/Menu.jsx
+++ src/layout/sidebar/Menu.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import style from './style.module.css'
+import {motion} from 'framer-motion/dist/es/index'
 
 
 function Menu() {
@@ -9,13 +10,13 @@ function Menu() {
 
   return (
     <aside>
-      <nav className={style.mobileNav}>
+      <motion.nav animate={{opacity:1, marginTop: 0}} initial={{opacity:0, marginTop:-100}} exit={{opacity:0, margintop:-100}} className={style.mobileNav}>
           <Link to='/'>Home</Link>
           <Link to='/services'>Services</Link>
           <Link to='/aboutus'>About Us</Link>
           <Link to='/gallery'>Gallery</Link>
           <Link to='/contactus' className={style.contact}>Contact Us</Link>
-        </nav>
+        </motion.nav>
     </aside>
   )
 }
